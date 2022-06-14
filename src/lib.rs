eceb14002bdd28baecf26f17ac8f4362a28c6605
kodylow <kodylow7@gmail.com>
2022-06-14 09:46:50 -0700

    multiple lifetime annotations

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,67 @@
-pub struct StrSplit<'a> {
-    remainder: &'a str,
-    delimiter: &'a str,
+// Lifetimes are for pointers
+// how long does the compiler need to keep it around?
+#[derive(Debug)]
+pub struct StrSplit<'haystack, 'delimiter> {
+    remainder: Option<&'haystack str>,
+    delimiter: &'delimiter str,
 }
 
-impl StrSplit<'_> {
-    pub fn new(haystack: &str, delimiter: &str) -> Self {
+// need lifetimes here, compiler will keep strsplit around for as long as
+// its inputs are around
+impl<'haystack, 'delimiter> StrSplit<'haystack, 'delimiter> {
+    pub fn new(haystack: &'haystack str, delimiter: &'delimiter str) -> Self {
         Self {
-            remainder: haystack,
+            remainder: Some(haystack),
             delimiter,
         }
     }
 }
 
-impl<'a> Iterator for StrSplit<'a> {
+impl<'haystack, 'delimiter> Iterator for StrSplit<'haystack, 'delimiter> {
     // need to specify lifetime in Iterator because next returns a pointer
     // how long does it hold onto it?
-    type Item = &'a str;
+    type Item = &'haystack str;
     fn next(&mut self) -> Option<Self::Item> {
+        let remainder = self.remainder.as_mut()?;
         if let Some(next_delim) = self.remainder.find(self.delimiter) {
-            let until_delimiter = &self.remainder[..next_delim];
-            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+            let until_delimiter = &remainder[..next_delim];
+            *remainder = &remainder[(next_delim + self.delimiter.len())..];
             Some(until_delimiter)
-        } else if self.remainder.is_empty() {
-            // TODO: bug
-            None
         } else {
-            let rest = self.remainder;
-            self.remainder = &[];
-            Some(rest)
+            self.remainder.take()
         }
     }
+}
+
+fn until_char(s: &str, c: char) -> &str {
+    let delim = format!("{}", c);
+    StrSplit::new(s, &delim)
+        .next()
+        .expect("StrSplit always returns at least one result")
+}
+
+#[test]
+fn until_char_test() {
+    assert_eq!(until_char("hello world", 'o'), "hell");
 }
 
 #[test]
 fn it_works() {
     let haystack = "a b c d e";
-    let letters = StrSplit::new(haystack, " ").collect();
-    assert_eq!(letters, vec!["a", "b", "c", "d", "e"].into_iter());
+    let letters = StrSplit::new(haystack, " ");
+    assert!(letters.eq(vec!["a", "b", "c", "d", "e"].into_iter()));
+}
+
+#[test]
+fn tail() {
+    let haystack = "a b c d ";
+    let letters = StrSplit::new(haystack, " ");
+    assert!(letters.eq(vec!["a", "b", "c", "d", ""].into_iter()));
+}
+
+#[test]
+fn empty_tail() {
+    let haystack = "a b c d ";
+    let letters = StrSplit::new(haystack, " ");
+    assert!(letters.eq(vec!["a", "b", "c", "d", ""].into_iter()));
 }
