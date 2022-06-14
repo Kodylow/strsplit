06d4e98a6e723d9bf8fde83ad1bba02b5ade6725
kodylow <kodylow7@gmail.com>
2022-06-13 21:15:22 -0700

    anonymous lifetime attempts, only work if theres only one option for the lifetime apparently

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,40 @@
-//!
-#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
+}
 
-pub struct StrSplit {}
+impl StrSplit<'_> {
+    pub fn new(haystack: &str, delimiter: &str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
+    }
+}
 
-impl StrSplit {
-    pub fn new(haystack: &str, delimiter: &str) -> Self {}
+impl<'a> Iterator for StrSplit<'a> {
+    // need to specify lifetime in Iterator because next returns a pointer
+    // how long does it hold onto it?
+    type Item = &'a str;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
+        } else if self.remainder.is_empty() {
+            // TODO: bug
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = &[];
+            Some(rest)
+        }
+    }
+}
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"].into_iter());
 }
